ce6e12f1cc13f977a334c092e4819ee86da93170
Krawk013 <109297452+Krawk013@users.noreply.github.com>
2024-03-26 08:49:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const backButton = document.getElementById('backButton');
 const forwardButton = document.getElementById('forwardButton');
-const urlInput = document.getElementById('urlInput');
-const goButton = document.getElementById('goButton');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
 const contentFrame = document.getElementById('contentFrame');
 
 backButton.addEventListener('click', () => {
@@ -12,8 +12,8 @@ forwardButton.addEventListener('click', () => {
     contentFrame.contentWindow.history.forward();
 });
 
-goButton.addEventListener('click', () => {
-    const searchTerm = encodeURIComponent(urlInput.value.trim());
+searchButton.addEventListener('click', () => {
+    const searchTerm = encodeURIComponent(searchInput.value.trim());
     const searchUrl = `https://www.google.com/search?q=${searchTerm}`;
     contentFrame.src = searchUrl;
 });
